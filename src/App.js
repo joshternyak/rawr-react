e4b0360ab20254e49a54281cbcc76c48bd33c1e1
Josh Ternyak <joshternyak@gmail.com>
2021-03-08 14:10:44 -0600

    Added react router to app

--- src/App.js
+++ src/App.js
@@ -4,43 +4,93 @@ import ComponentDetails from './ComponentDetails';
 import { componentsList } from './componentsList';
 import Grid from './ComponentsDirectory/Grid';
 import { ThemeContext } from './contexts/theme';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
-  const [activeTab, setActiveTab] = useState(0);
   const { theme, toggle, dark } = React.useContext(ThemeContext);
+  const navbarLinkStyle = { color: dark ? '#fff' : 'black' };
 
   return (
-    <div className="App" style={{
-      backgroundColor: theme.backgroundColor,
-      color: theme.color,
-    }}>
-      <Grid justifyContent={'space-between'} alignItems={'start'}>
-        <ComponentTabsList
-          title={' < Rawr /> React Components'}
-          setActiveTab={setActiveTab}
-          activeTab={activeTab}
-        />
-        <div>
+    <Router>
+      <div className="App" style={{
+        backgroundColor: theme.backgroundColor,
+        color: theme.color,
+      }}>
+        <div className="App__navbar">
+          <ul className="App__navbar-list">
+            <li className="App__navbar-list-item">
+              <Link
+                style={navbarLinkStyle}
+                exact to='/'
+                className="App__navbar-list-item-link"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="App__navbar-list-item">
+              <Link
+                style={navbarLinkStyle}
+                to='/about'
+                className="App__navbar-list-item-link"
+              >
+                About
+              </Link>
+            </li>
+            <li className="App__navbar-list-item">
+              <Link
+                style={navbarLinkStyle}
+                to='/contact'
+                className="App__navbar-list-item-link"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+
           <button className="App__toggle-theme-btn" onClick={toggle}>
             {!dark ? '🔦' : '💡'}
           </button>
-          {componentsList.map((componentItem, index) => {
-            const isActiveTab = activeTab === index;
-            return isActiveTab &&
-              <div key={index}>
-                <ComponentDetails
-                  description={componentItem.description}
-                  name={componentItem.name}
-                  whenToUse={componentItem.whenToUse}
-                  howToUse={componentItem.howToUse}
-                  examples={componentItem.examples}
-                />
-              </div>
-              ;
-          })}
         </div>
-      </Grid>
-    </div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <h2 className="App__info-page-title">About Us</h2>
+          </Route>
+          <Route path="/contact">
+            <h2 className="App__info-page-title">Contact</h2>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  const [activeTab, setActiveTab] = useState(0);
+  return (
+    <Grid justifyContent={'space-between'} alignItems={'start'}>
+      <ComponentTabsList
+        title={' < Rawr /> React Components'}
+        setActiveTab={setActiveTab}
+        activeTab={activeTab}
+      />
+      {componentsList.map((componentItem, index) => {
+        const isActiveTab = activeTab === index;
+        return isActiveTab &&
+          <div key={index}>
+            <ComponentDetails
+              description={componentItem.description}
+              name={componentItem.name}
+              whenToUse={componentItem.whenToUse}
+              howToUse={componentItem.howToUse}
+              examples={componentItem.examples}
+            />
+          </div>
+          ;
+      })}
+    </Grid>
   );
 }
 
